fc3b20e5e0ae73e531eb909bd021bed79fc5b2b4
DongJin Kim <31959020+ywydpapa@users.noreply.github.com>
2025-05-09 01:59:21 +0900

    코인트렌드 추가

--- aiTrader/cprice.py
+++ aiTrader/cprice.py
@@ -1,5 +1,5 @@
 import requests
-from datetime import datetime, UTC
+from datetime import datetime, timezone
 
 def all_cprice():
     server_url = "https://api.upbit.com"
@@ -15,7 +15,7 @@ def all_cprice():
         trade_price = item.get("trade_price")
         timestamp = item.get("timestamp")
         if market and trade_price and timestamp:
-            time_str = datetime.fromtimestamp(timestamp / 1000, UTC ).strftime('%Y-%m-%d %H:%M:%S')
+            time_str = datetime.fromtimestamp(timestamp / 1000, timezone.utc ).strftime('%Y-%m-%d %H:%M:%S')
             result.append({"market": market,"trade_price": trade_price,"timestamp": timestamp, "time_str": time_str })
 
     return result
